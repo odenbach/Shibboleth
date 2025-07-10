bde9f77209dc380d653272e00f7555a702ca4dac
Christopher Odenbach <odenbach@uni-paderborn.de>
2025-07-10 13:59:11 +0200

    avoid warnings about missing aliases

---
+++ Shibboleth.alias.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Aliases for invisible Shibboleth special pages
+ *
+ * There is no need to translate the messages, since the PageTitle does not appear.
+ *
+$specialPageAliases = [];
+
+/** English (English) */
+$specialPageAliases['en'] = [
+	'Shibboleth' => [ 'Shibboleth' ]
+];
+
+?>
